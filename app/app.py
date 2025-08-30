6c5871a781bccf8865b80416aa61dcd092567402
JoJoSapiens <olegforkyrat2003@gmail.com>
2025-08-30 21:33:02 +0300

    добавил класс записи о погоде и сгенерированные данные

--- app/app.py
+++ app/app.py
@@ -1,12 +1,25 @@
 import tkinter as tk
 from tkinter import ttk
 
+import pandas as pd
+
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Прогноз погоды")
         self.geometry("1600x1200")
+
+
+class WeatherRecord:
+    def __init__(self, date, city, t_min, t_max, t_avg, description):
+        self.date = pd.to_datetime(date)
+        self.city = city
+        self.t_min = float(t_min)
+        self.t_max = float(t_max)
+        self.t_avg = float(t_avg)
+        self.description = description
+        self.swing = self.t_max - self.t_min
 
 
 if __name__ == "__main__":
